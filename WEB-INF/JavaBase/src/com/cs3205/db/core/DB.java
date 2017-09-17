1ea5fbf43fc329b763ee4a3ae52a6728e7400c3c
Jia Ming <jiaming@picoded.com>
2017-09-18 02:07:48 +0800

    Tomcat apache friendly

---
+++ WEB-INF/JavaBase/src/com/cs3205/db/core/DB.java
@@ -0,0 +1,47 @@
+package com.cs3205.db.core;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.Context;
+import javax.sql.DataSource;
+
+public class DB{
+  private static DataSource datasource = null;
+  private static Context initContext = null;
+  private static Connection conn = null;
+  private static String connectURL = "";
+
+  public static void setConfiguration(String connectionURL){
+    connectURL = connectionURL;
+  }
+
+  public static DataSource datasource(){
+    if (datasource != null) {
+      return datasource;
+    }
+    establishConnection();
+    return datasource;
+  }
+
+  public static void establishConnection(){
+    if (datasource == null){
+      try{
+        initContext = new InitialContext();
+        // connection = "java:comp/env/jdbc/TestDB"
+        datasource = (DataSource) initContext.lookup(connectURL);
+      }catch(Exception e){
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static Connection getConnection() throws SQLException{
+    if (conn != null){
+      return conn;
+    }
+    return (conn = datasource().getConnection());
+  }
+
+}
