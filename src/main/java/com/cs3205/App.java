fffb888d20effd31d6055f20d46be8923baa9736
Jia Ming <jiaming@picoded.com>
2017-09-19 17:56:31 +0800

    init maven project

---
+++ src/main/java/com/cs3205/App.java
@@ -0,0 +1,13 @@
+package com.cs3205;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/cs3205/api/session/core/Session.java
@@ -0,0 +1,6 @@
+package cs3205.api.session.core;
+
+public interface Session{
+  public void upload();
+  public String get();
+}
