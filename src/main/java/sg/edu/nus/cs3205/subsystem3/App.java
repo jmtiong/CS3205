ae422e128433e6bbc5380d0ed1ed6a3fced5037a
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-09-27 17:19:10 +0800

    Clean up package name

---
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/App.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.cs3205.subsystem3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/dao/JDataObject.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.cs3205.subsystem3.dao;
+
+import java.util.HashMap;
+
+public class JDataObject extends HashMap<String, Object> {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -2980872095666604706L;
+    String oid = "";
+
+    public JDataObject() {
+        super();
+    }
+
+}
