6d19825de60d1ef6a48058f30a9e70469e724512
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-09-27 21:47:00 +0800

    First working barebone server

--- src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/HeartSession.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/HeartSession.java
@@ -2,26 +2,20 @@ package sg.edu.nus.cs3205.subsystem3.api.session;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import sg.edu.nus.cs3205.subsystem3.api.session.core.Session;
-
-@Path("/heart")
+@Produces(MediaType.APPLICATION_JSON)
 public class HeartSession implements Session {
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Override
     public String get() {
-        return "test";
+        return "\"You sent a GET request\"";
     }
 
     @POST
-    @Path("/upload")
-    @Produces(MediaType.TEXT_PLAIN)
     @Override
-    public void upload() {
-        System.out.println("Testing");
+    public String upload(String body) {
+        return "\"You sent " + body + '"';
     }
 }
