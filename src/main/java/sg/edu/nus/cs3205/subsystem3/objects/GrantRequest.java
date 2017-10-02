53fea0cc0a19d7703f3a138012b682467ff57310
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-02 23:03:50 +0800

    Finish /oauth/token stub

--- src/main/java/sg/edu/nus/cs3205/subsystem3/objects/GrantRequest.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/objects/GrantRequest.java
@@ -1,36 +1,13 @@
 package sg.edu.nus.cs3205.subsystem3.objects;
 
 public class GrantRequest {
-    private GrantType grantType;
-    private String username;
-    private String passhash;
+    public GrantType grantType;
+    public String username;
+    public String passhash;
 
+    // TODO: handle JsonMappingException when inputting an integer out of range
     public static enum GrantType {
         PASSWORD;
-    }
-
-    public GrantType getGrantType() {
-        return grantType;
-    }
-
-    public void setGrantType(GrantType grantType) {
-        this.grantType = grantType;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasshash() {
-        return passhash;
-    }
-
-    public void setPasshash(String passhash) {
-        this.passhash = passhash;
     }
 
     public String getPasswordClaim(long expiration) {
