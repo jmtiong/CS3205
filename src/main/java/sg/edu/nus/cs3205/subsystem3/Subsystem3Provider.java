171a2d8642c508bc56816bc57188b9d029195363
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-02 22:50:25 +0800

    Add custom JsonProcessingException response Mapper

--- src/main/java/sg/edu/nus/cs3205/subsystem3/Subsystem3Provider.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/Subsystem3Provider.java
@@ -1,20 +1,30 @@
 package sg.edu.nus.cs3205.subsystem3;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
-@javax.ws.rs.ext.Provider
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public class Provider extends JacksonJsonProvider {
-    public Provider() {
+import sg.edu.nus.cs3205.subsystem3.objects.ErrorResponse;
+
+@Provider
+public class Subsystem3Provider extends JacksonJsonProvider implements ExceptionMapper<JsonProcessingException> {
+    public Subsystem3Provider() {
         super(new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS));
+                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+                .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL));
+    }
+
+    @Override
+    public Response toResponse(JsonProcessingException exception) {
+        return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorResponse(exception.getMessage()))
+                .type(MediaType.APPLICATION_JSON).build();
     }
 }
