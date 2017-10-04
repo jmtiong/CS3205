bd95e175ffd293b2a4ee1f2cb1757376f19aed0e
Jia Ming <jiaming@picoded.com>
2017-10-04 17:37:22 +0800

    Added if else checks for different types of requests

--- src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/HeartSession.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/HeartSession.java
@@ -2,6 +2,7 @@ package sg.edu.nus.cs3205.subsystem3.api.session;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import sg.edu.nus.cs3205.subsystem3.dao.DBQueryParser;
@@ -35,7 +36,10 @@ public class HeartSession implements Session {
 
     @POST
     @Override
+    @Path("/whatever/whatever")
     public String upload(String body) {
+        // Authentication of the user
+        // Request
         return "\"You sent " + body + '"';
     }
 }
