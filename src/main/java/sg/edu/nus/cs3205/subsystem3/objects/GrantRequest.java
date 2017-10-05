f0c2874cc972e1877dd6a5b9ed4e267b116617ea
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-05 17:06:42 +0800

    Use json ObjectMapper for jwt payload; use userId in claim

--- src/main/java/sg/edu/nus/cs3205/subsystem3/objects/GrantRequest.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/objects/GrantRequest.java
@@ -2,15 +2,30 @@ package sg.edu.nus.cs3205.subsystem3.objects;
 
 public class GrantRequest {
     public GrantType grantType;
+    public Integer userId;
     public String username;
     public String passhash;
+
+    @SuppressWarnings("unused")
+    private static final class GrantClaim {
+        public int userId;
+        public long exp;
+
+        public GrantClaim(int userId, long expiration) {
+            this.userId = userId;
+            this.exp = expiration;
+        }
+    }
 
     // TODO: handle JsonMappingException when inputting an integer out of range
     public static enum GrantType {
         PASSWORD;
     }
 
-    public String getPasswordClaim(long expiration) {
-        return String.format("{\"username\":\"%s\",\"exp\":%d}", username, expiration);
+    public GrantClaim getPasswordClaim(long expiration) {
+        if (userId == null) {
+            throw new IllegalStateException("userId hasn't been populated");
+        }
+        return new GrantClaim(userId, expiration);
     }
 }
