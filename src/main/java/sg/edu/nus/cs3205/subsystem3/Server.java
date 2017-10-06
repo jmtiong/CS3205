19a048f86b6494e1eec5e1b545511536edfb9c2f
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-07 01:07:36 +0800

    Use existing constants for headers and methods

--- src/main/java/sg/edu/nus/cs3205/subsystem3/Server.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/Server.java
@@ -3,9 +3,11 @@ package sg.edu.nus.cs3205.subsystem3;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -24,9 +26,9 @@ public class Server {
 
     @GET
     public Response getRoot(@Context final UriInfo uri) {
-        return Response.ok(new Links(Links.newLink(uri, "", "self", "GET"),
-                Links.newLink(uri, "oauth/token", "oauth.token", "POST"),
-                Links.newLink(uri, "session", "session", "POST"))).build();
+        return Response.ok(new Links(Links.newLink(uri, "", "self", HttpMethod.GET),
+                Links.newLink(uri, "oauth/token", "oauth.token", HttpMethod.POST),
+                Links.newLink(uri, "session", "session", HttpMethod.POST))).build();
     }
 
     @Path("/oauth/token")
@@ -35,7 +37,7 @@ public class Server {
     }
 
     @Path("/session")
-    public Session session(@HeaderParam("Authorization") final String accessToken,
+    public Session session(@HeaderParam(HttpHeaders.AUTHORIZATION) final String accessToken,
             @HeaderParam("X-NFC-Token") final String nfcToken) {
         GrantClaim claim;
         if (accessToken == null) {
