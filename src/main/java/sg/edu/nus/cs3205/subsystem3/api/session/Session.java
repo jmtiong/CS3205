0b2bcb664cba5c97a37084fee7fcbcaa521d86f8
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-06 16:25:01 +0800

    Fix ClosedStream bug for heartrate upload case

--- src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/Session.java
+++ src/main/java/sg/edu/nus/cs3205/subsystem3/api/session/Session.java
@@ -56,7 +56,7 @@ public class Session {
         }
     }
 
-    private static final String RESOURCE_SERVER_SESSION_PATH = "http://cs3205-4-i.comp.nus.edu.sg/api/team3";;
+    private static final String RESOURCE_SERVER_SESSION_PATH = "http://cs3205-4-i.comp.nus.edu.sg/api/team3";
 
     private final Integer userID;
 
@@ -114,8 +114,8 @@ public class Session {
         final Invocation.Builder client = ClientBuilder.newClient().target(target).request();
         System.out.println("POST " + target);
         // TODO Add in the headers for server 4 verification in the future
-        final Response response = client
-                .post(Entity.entity(requestStream, MediaType.APPLICATION_OCTET_STREAM_TYPE));
+        final Response response = client.post(Entity.entity(type == SessionType.HEART ? null : requestStream,
+                MediaType.APPLICATION_OCTET_STREAM_TYPE));
         // TODO Custom response
         return response;
     }
