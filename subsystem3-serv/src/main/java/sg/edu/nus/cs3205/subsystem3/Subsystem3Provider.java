3ef2256321929d581ff8443d52e0834ebe520f1e
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-30 15:05:31 +0800

    More specific Exception mapper; static mapper; use logger

--- subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/Subsystem3Provider.java
+++ subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/Subsystem3Provider.java
@@ -1,6 +1,9 @@
 package sg.edu.nus.cs3205.subsystem3;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -11,8 +14,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
@@ -22,14 +25,20 @@ import sg.edu.nus.cs3205.subsystem3.pojos.ErrorResponse;
 
 @Provider
 public class Subsystem3Provider extends JacksonJsonProvider
-        implements ExceptionMapper<JsonProcessingException>, ContainerRequestFilter {
+        implements ExceptionMapper<JsonMappingException>, ContainerRequestFilter {
+    private static final Logger LOGGER = Logger.getLogger(Subsystem3Provider.class.getName());
+
     @Context
     private HttpServletRequest request;
 
+    private static ObjectMapper objectMapper = new ObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+
     public Subsystem3Provider() {
-        super(new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-                .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL));
+        super(objectMapper);
+        LOGGER.fine("Registered provider " + this.getClass().getName());
     }
 
     @Override
@@ -42,13 +51,15 @@ public class Subsystem3Provider extends JacksonJsonProvider
         if (local == null) {
             local = request.getLocalAddr();
         }
-        System.out.printf("%s from %s:%d to %s:%d\n", requestContext.getDate(), remote,
-                request.getRemotePort(), local, request.getLocalPort());
+        LOGGER.info(String.format("%s from %s:%d to %s:%d\n", new Date(), remote, request.getRemotePort(),
+                local, request.getLocalPort()));
     }
 
     @Override
-    public Response toResponse(final JsonProcessingException exception) {
-        return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorResponse(exception.getMessage()))
-                .type(MediaType.APPLICATION_JSON_TYPE).build();
+    public Response toResponse(final JsonMappingException exception) {
+        LOGGER.log(Level.FINE, "JsonParseException", exception);
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(new ErrorResponse("Invalid body's JSON format")).type(MediaType.APPLICATION_JSON_TYPE)
+                .build();
     }
 }
