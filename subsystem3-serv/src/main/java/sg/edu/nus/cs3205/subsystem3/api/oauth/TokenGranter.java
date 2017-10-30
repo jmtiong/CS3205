7698a023b4f730e52afea5d6b62645a3088d4dde
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-10-30 15:04:11 +0800

    Refactor + add NFC c-r auth scheme

--- subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/api/oauth/TokenGranter.java
+++ subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/api/oauth/TokenGranter.java
@@ -1,9 +1,6 @@
 package sg.edu.nus.cs3205.subsystem3.api.oauth;
 
 import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -15,29 +12,22 @@ import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 import sg.edu.nus.cs3205.subsystem3.exceptions.WebException;
+import sg.edu.nus.cs3205.subsystem3.pojos.AuthChallenge;
+import sg.edu.nus.cs3205.subsystem3.pojos.ErrorResponse;
 import sg.edu.nus.cs3205.subsystem3.pojos.GrantRequest;
 import sg.edu.nus.cs3205.subsystem3.pojos.PasswordGrant;
 import sg.edu.nus.cs3205.subsystem3.pojos.GrantRequest.GrantType;
+import sg.edu.nus.cs3205.subsystem3.util.HttpHeaders;
+import sg.edu.nus.cs3205.subsystem3.util.ResourceServerConnector;
 import sg.edu.nus.cs3205.subsystem3.util.security.TokenUtils;
-
-import sg.edu.nus.cs3205.subsystem3.dao.DB;
-
-
-// Challenge Response Implementation imports
-import java.util.Base64;
-import java.util.Arrays;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class TokenGranter {
+
     static final class SupportedGrantTypes {
         public GrantType[] supportedGrantTypes = GrantRequest.GrantType.values();
     }
@@ -48,108 +38,56 @@ public class TokenGranter {
     }
 
     @POST
-    public Response grant(final GrantRequest request, @HeaderParam("X-NFC-Token") final String nfcToken, @HeaderParam("Authorization") final String authorizationHeader, @HeaderParam("debug")boolean debugMode) {
+    public Response grant(final GrantRequest request,
+            @HeaderParam(HttpHeaders.X_NFC_RESPONSE) final String nfcResponse,
+            @HeaderParam(HttpHeaders.AUTHORIZATION) final String authorizationHeader,
+            @HeaderParam("debug") boolean debugMode) throws InvalidKeyException, JsonProcessingException {
         if (request == null) {
             throw new WebException(Response.Status.BAD_REQUEST, "Missing request body");
+        } else if (request.grantType == null) {
+            throw new WebException(Response.Status.BAD_REQUEST, "Missing/invalid grant_type field");
+        } else if (request.username == null) {
+            throw new WebException(Response.Status.BAD_REQUEST, "Missing username field");
         }
-        System.out.println("debugMode: "+debugMode);
-        if (request.grantType == null) {
-            throw new WebException(Response.Status.BAD_REQUEST, "Missing/invalid grant_type");
-        } else if (request.grantType == GrantType.PASSWORD) {
-            if (request.username != null && authorizationHeader == null) {
-                int status = 401;
-                ObjectMapper mapper = new ObjectMapper();
-                ObjectNode jObj = mapper.createObjectNode();
-                // get challenge and salt from server 4
-                String challenge = getChallenge(request.username);
-                String salt = getUserSalt(request.username);
-                jObj.put("challenge", challenge);
-                jObj.put("salt", salt);
-                if(challenge.isEmpty() || salt.isEmpty()){
-                  return Response.status(status).entity("Server unable to generate challenge for unknown user.").build();
+
+        if (request.grantType == GrantType.PASSWORD) {
+            // Get challenge
+            if (authorizationHeader == null) {
+                // Get challenge and salt from server 4
+                // TODO async request
+                String challenge = ResourceServerConnector.getChallenge(request.username);
+                String salt = ResourceServerConnector.getUserSalt(request.username);
+                AuthChallenge authChallenge = new AuthChallenge(challenge, salt);
+                return Response.status(Response.Status.UNAUTHORIZED).header(HttpHeaders.WWW_AUTHENTICATE,
+                        new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                                .writeValueAsString(authChallenge))
+                        .header(HttpHeaders.X_NFC_CHALLENGE,
+                                ResourceServerConnector.getNFCChallenge(request.username))
+                        .entity(new ErrorResponse(Response.Status.UNAUTHORIZED.getReasonPhrase())).build();
+            } else {
+                // User attempts to log in
+                String[] authHeader = authorizationHeader.split(" ");
+                if (authHeader.length < 2) {
+                    throw new WebException(Response.Status.BAD_REQUEST, "Invalid Authorization Header.");
                 }
-                // Return back www-authenticate header
-                try{
-                  return Response.status(status)
-                          .header("www-authenticate", mapper.writeValueAsString(jObj))
-                          .entity("Missing Authorization Header.").build();
-                } catch(Exception e){
-                  e.printStackTrace();
-                  status = 500;
+                if (nfcResponse == null) {
+                    throw new WebException(Response.Status.BAD_REQUEST, "Missing X-NFC-Response header");
                 }
-                return Response.status(status).entity("Server unable to generate challenge for unknown user.").build();
-            }
-            // User attempts to log in
-            if (request.username != null && authorizationHeader != null) {
-              if (nfcToken == null) {
-                  throw new WebException(Response.Status.BAD_REQUEST, "Missing X-NFC-Token header");
-              }
-              String[] authHeader = authorizationHeader.split(" ");
-              if(authHeader.length < 2){
-                throw new WebException(Response.Status.BAD_REQUEST, "Invalid Authorization Header.");
-              }
-                boolean answer = loginUser(request.username, authorizationHeader, nfcToken);
-                if(answer || debugMode){
-                  try {
-                      request.userId = 1;
-                      final String jwt = TokenUtils.createJWT(request);
-                      return Response.ok(new PasswordGrant(jwt)).build();
-                  } catch (JsonProcessingException | InvalidKeyException e) {
-                      throw new WebException(e);
-                  }
+                if (debugMode) {
+                    request.userId = 1;
+                } else {
+                    request.userId = ResourceServerConnector.verifyResponse(request.username,
+                            authorizationHeader, nfcResponse);
+                }
+                try {
+                    final String jwt = TokenUtils.createJWT(request);
+                    return Response.ok(new PasswordGrant(jwt)).header(HttpHeaders.SET_AUTHORIZATION, jwt)
+                            .build();
+                } catch (JsonProcessingException | InvalidKeyException e) {
+                    throw new WebException(e);
                 }
             }
-            throw new WebException(Response.Status.UNAUTHORIZED, "Invalid credential");
         }
-        throw new WebException(Response.Status.BAD_REQUEST, "Unknown %s grant_type", request.grantType);
-    }
-
-    private static final String RESOURCE_SERVER_SESSION_PATH = "http://cs3205-4-i.comp.nus.edu.sg/api/team3";
-
-    private String getUserSalt(String username){
-      final String target;
-          target = String.format("%s/%s?username=%s", RESOURCE_SERVER_SESSION_PATH,
-                  "user", username, "salt2");
-      final Invocation.Builder client = ClientBuilder.newClient().target(target).request();
-      System.out.println("GET " + target);
-      // TODO Add in the headers for server 4 verification in the future
-      final Response response = client.get();
-      String rawResponse = response.readEntity(String.class);
-      if(response.getStatus() == 401) {
-        rawResponse = "";
-      }
-      return rawResponse;
-    }
-
-    private String getChallenge(String username){
-      final String target;
-          target = String.format("%s/%s/%s?username=%s", RESOURCE_SERVER_SESSION_PATH,
-                  "user", "challenge", username);
-      final Invocation.Builder client = ClientBuilder.newClient().target(target).request();
-      System.out.println("GET " + target);
-      final Response response = client.get();
-      String rawResponse = response.readEntity(String.class);
-      if(response.getStatus() == 401) {
-        rawResponse = "";
-      }
-      return rawResponse;
-    }
-
-    private boolean loginUser(String username, String authorizationHeader, String nfcToken){
-      System.out.println(username+" "+authorizationHeader+" "+nfcToken);
-      final String target;
-          target = String.format("%s/%s/%s?username=%s", RESOURCE_SERVER_SESSION_PATH,
-                  "user", "login", username);
-      final Invocation.Builder client = ClientBuilder.newClient().target(target).request();
-      System.out.println("POST " + target);
-      final Response response = client
-                                  .header("Authorization", authorizationHeader)
-                                  .header("X-NFC-Token", nfcToken)
-                                  .post(null);
-      if(response.getStatus() == 201){
-        return true;
-      }
-      return false;
-
+        throw new WebException(Response.Status.BAD_REQUEST, "Unsupported %s grant_type", request.grantType);
     }
 }
