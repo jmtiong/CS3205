c822ecbb79150edfa9888c239c5949e83dff1b12
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-11-02 00:46:35 +0800

    Fix problem with #fromResponse copying over control headers

--- subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/util/HttpHeaders.java
+++ subsystem3-serv/src/main/java/sg/edu/nus/cs3205/subsystem3/util/HttpHeaders.java
@@ -1,6 +1,7 @@
 package sg.edu.nus.cs3205.subsystem3.util;
 
 public interface HttpHeaders extends javax.ws.rs.core.HttpHeaders {
+    String CONNECTION = "Connection";
     String X_NFC_CHALLENGE = "X-NFC-Challenge";
     String X_NFC_RESPONSE = "X-NFC-Response";
     String X_PASSWORD_RESPONSE = "X-Password-Response";
